8457ab36eaa45089a30b489526f1be7881fdb432
ionut.todoran <Softvision10>
2020-02-17 15:55:50 +0200

    First Person Commit

---
+++ src/test/java/com/example/LProject/LProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
