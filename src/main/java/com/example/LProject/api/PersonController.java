9fc58958e8d3b0686e1242c6482460e4258d0849
ionut.todoran <Softvision10>
2020-02-17 18:29:45 +0200

    GET POST add person to DB

---
+++ src/main/java/com/example/LProject/api/PersonController.java
@@ -0,0 +1,31 @@
+package com.example.LProject.api;
+
+import com.example.LProject.model.Person;
+import com.example.LProject.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople()
+    {
+        return personService.getAllPeople();
+    }
+}
