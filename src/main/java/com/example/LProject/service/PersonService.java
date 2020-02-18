edc1b7e90d4a036749a338d851d6837a7cf89a05
ionut.todoran <Softvision10>
2020-02-18 15:17:35 +0200

    UPDATE person

--- src/main/java/com/example/LProject/service/PersonService.java
+++ src/main/java/com/example/LProject/service/PersonService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PersonService {
@@ -22,8 +24,21 @@ public class PersonService {
         return personDao.insertPerson(person);
     }
 
-
     public List<Person> getAllPeople() {
         return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id)
+    {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id){
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson)
+    {
+        return personDao.updatePersonById(id, newPerson);
     }
 }
